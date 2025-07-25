bcc9cede25a3478823e662ddb0e0506b5e321c4a
hsthe <hsthe@rd.misa.com.vn>
2025-07-25 14:02:25 +0700

    ok

--- pipeline.sh
+++ pipeline.sh
@@ -3,6 +3,6 @@
 pip install -U packaging==23.2 setuptools==75.8.0 wheel ninja
 pip install --no-build-isolation axolotl[flash-attn,deepspeed]
 
-# accelerate launch --config_file configs/pretraining/setup.yaml -m axolotl.cli.train configs/pretraining/axolotl.yaml
+accelerate launch --config_file configs/pretraining/setup.yaml -m axolotl.cli.train configs/pretraining/debug.yaml
 
-torchrun --nproc_per_node=4 -m axolotl.cli.train configs/pretraining/debug.yaml
+# torchrun --nproc_per_node=4 -m axolotl.cli.train configs/pretraining/debug.yaml
