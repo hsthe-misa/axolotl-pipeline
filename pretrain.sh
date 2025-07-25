a16070606dcf315e402a9732f43caa60cc15def9
hsthe <hsthe@rd.misa.com.vn>
2025-07-25 23:27:11 +0700

    ok

--- pretrain.sh
+++ pretrain.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-torchrun --nproc_per_node=4 -m axolotl.cli.train configs/pretraining/axolotl.yaml
+# torchrun --nproc_per_node=4 -m axolotl.cli.train configs/pretraining/axolotl.yaml
+axolotl train configs/pretraining/axolotl.yaml
